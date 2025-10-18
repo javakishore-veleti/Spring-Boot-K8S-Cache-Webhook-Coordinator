655bbf29877cb27d12816bdbca4b1460c5e4cec4
javakishore-veleti <javakishore@gmail.com>
2025-10-17 20:44:42 -0400

    Initial commit

---
+++ src/main/java/com/jk/ref_impl/cache_webhook/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.jk.ref_impl.cache_webhook.config;
+
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class AppConfig {
+}
